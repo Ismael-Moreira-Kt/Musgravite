9e57e8bbd82fff1026d44fc78d5976ee0fe8612f
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-15 14:53:55 +0100

    feat: Creating musgravite.hpp
    I've added the definition of the colors that will be used in the project.

---
+++ musgravite/musgravite.hpp
@@ -0,0 +1,31 @@
+#ifndef MUSGRAVITE_HPP
+#define MUSGRAVITE_HPP
+
+
+
+
+
+#include <iostream>
+#include <vector>
+#include <functional>
+#include <stdexcept>
+#include <sstream>
+#include <chrono>
+#include <cmath>
+#include <map>
+#include <future>
+#include <thread>
+#include <iomanip>
+
+
+
+
+
+#define RESET   "\033[0m"
+#define RED     "\033[31m"
+#define GREEN   "\033[32m"
+#define YELLOW  "\033[33m"
+#define BLUE    "\033[34m"
+#define MAGENTA "\033[35m"
+#define CYAN    "\033[36m"
+#define BOLD    "\033[1m"

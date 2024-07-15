5e78b37008a69fd9512b3bb718ee193e7af36e47
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-15 19:33:15 +0100

    feat: Creation of musgravite.h.
    I've added the definition of the MusgraviteRunner class implementation.

---
+++ musgravite/C Wrapper/musgravite.h
@@ -0,0 +1,26 @@
+#ifndef MUSGRAVITE_H
+#define MUSGRAVITE_H
+
+
+
+
+
+#ifdef __cplusplus
+    extern "C" {
+        #endif
+
+
+
+            typedef struct MusgraviteRunnerImpl MusgraviteRunnerImpl;
+
+
+
+        #ifdef
+    }
+#endif
+
+
+
+
+
+#endif

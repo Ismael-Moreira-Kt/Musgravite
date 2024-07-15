1ac356b4c88463b293cb341399a097d6313c88ce
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-15 20:08:48 +0100

    feat: Edition of musgraviteImpl.cpp
    I've added the compatibility layer for assert macros.

--- musgravite/C Wrapper/musgraviteImpl.cpp
+++ musgravite/C Wrapper/musgraviteImpl.cpp
@@ -138,4 +138,92 @@ extern "C" {
     void IGNORE_TEST_C(const char* test) {
         printColored(std::string("Test ") + test + " ignored.", YELLOW);
     }
+
+
+
+    void ASSERT_TRUE_C(int cond, const char* cond_str, const char* file, int line) {
+        if (!cond) {
+            std::stringstream ss;
+            ss << "Assertion failed: " << cond_str << ", file " << file << ", line " << line;
+            
+            throw std::runtime_error(ss.str());
+        }
+    }
+
+
+
+    void ASSERT_FALSE_C(int cond, const char* cond_str, const char* file, int line) {
+        if (cond) {
+            std::stringstream ss;
+            ss << "Assertion failed: " << cond_str << " is true, file " << file << ", line " << line;
+            
+            throw std::runtime_error(ss.str());
+        }
+    }
+
+
+
+    void ASSERT_EQUAL_C(int expected, int actual, const char* file, int line) {
+        if (expected != actual) {
+            std::stringstream ss;
+            ss << "Assertion failed: expected " << expected << ", got " << actual << ", file " << file << ", line " << line;
+            
+            throw std::runtime_error(ss.str());
+        }
+    }
+
+
+
+    void ASSERT_NOT_EQUAL_C(int expected, int actual, const char* file, int line) {
+        if (expected == actual) {
+            std::stringstream ss;
+            ss << "Assertion failed: expected not " << expected << ", but got " << actual << ", file " << file << ", line " << line;
+            
+            throw std::runtime_error(ss.str());
+        }
+    }
+
+
+
+    void ASSERT_NEAR_C(double expected, double actual, double tol, const char* file, int line) {
+        if (std::fabs(expected - actual) > tol) {
+            std::stringstream ss;
+            ss << "Assertion failed: expected near " << expected << ", got " << actual << ", with tolerance " << tol << ", file " << file << ", line " << line;
+            
+            throw std::runtime_error(ss.str());
+        }
+    }
+
+
+
+    void ASSERT_THROW_C(void (*statement)(void), const char* exception_type, const char* file, int line) {
+        try {
+            statement();
+            
+            std::stringstream ss;
+            ss << "Assertion failed: expected exception " << exception_type << ", but no exception thrown, file " << file << ", line " << line;
+            
+            throw std::runtime_error(ss.str());
+        } catch (const std::exception& e) {
+            
+        } catch (...) {
+            std::stringstream ss;
+            ss << "Assertion failed: expected exception " << exception_type << ", but a different exception was thrown, file " << file << ", line " << line;
+            
+            throw std::runtime_error(ss.str());
+        }
+    }
+
+
+
+    void ASSERT_NO_THROW_C(void (*statement)(void), const char* file, int line) {
+        try {
+            statement();
+        } catch (...) {
+            std::stringstream ss;
+            ss << "Assertion failed: expected no exception, but an exception was thrown, file " << file << ", line " << line;
+            
+            throw std::runtime_error(ss.str());
+        }
+    }
 }

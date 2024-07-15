e0e2723467b9942451104d8d94010c7979b742c5
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-15 19:40:24 +0100

    feat: Edition of musgravite.h.
    I've added the assert macro wrapper.

--- musgravite/C Wrapper/musgravite.h
+++ musgravite/C Wrapper/musgravite.h
@@ -28,8 +28,18 @@
             void ADD_TEST_SETUP_TEARDOWN_TIMEOUT_C(const char* suite, const char* test, void (*func)(void), void (*setup)(void), void (*teardown)(void), int timeout);
         
 
-        
+
             void IGNORE_TEST_C(const char* test);
+
+
+
+            void ASSERT_TRUE_C(int cond, const char* cond_str, const char* file, int line);
+            void ASSERT_FALSE_C(int cond, const char* cond_str, const char* file, int line);
+            void ASSERT_EQUAL_C(int expected, int actual, const char* file, int line);
+            void ASSERT_NOT_EQUAL_C(int expected, int actual, const char* file, int line);
+            void ASSERT_NEAR_C(double expected, double actual, double tol, const char* file, int line);
+            void ASSERT_THROW_C(void (*statement)(void), const char* exception_type, const char* file, int line);
+            void ASSERT_NO_THROW_C(void (*statement)(void), const char* file, int line);
         #ifdef
     }
 #endif

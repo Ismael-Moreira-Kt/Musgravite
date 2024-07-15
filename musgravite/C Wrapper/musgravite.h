1154bdf76e373009f52a9f6d0180413b7ba515bf
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-15 19:36:37 +0100

    feat: Edition of musgravite.h.
    I've added the macro wrappers to add tests.

--- musgravite/C Wrapper/musgravite.h
+++ musgravite/C Wrapper/musgravite.h
@@ -8,9 +8,8 @@
 #ifdef __cplusplus
     extern "C" {
         #endif
-
-
             typedef struct MusgraviteRunnerImpl MusgraviteRunnerImpl;
+
 
 
             void runAllTestsC(void);
@@ -18,6 +17,15 @@
             void cleanupMusgraviteRunner(void);
 
 
+
+            void ADD_TEST_C(const char* suite, const char* test, void (*func)(void));
+            void ADD_TEST_TIMEOUT_C(const char* suite, const char* test, void (*func)(void), int timeout);
+            void ADD_TEST_SETUP_C(const char* suite, const char* test, void (*func)(void), void (*setup)(void));
+            void ADD_TEST_SETUP_TEARDOWN_C(const char* suite, const char* test, void (*func)(void), void (*setup)(void), void (*teardown)(void));
+            void ADD_TEST_SETUP_TIMEOUT_C(const char* suite, const char* test, void (*func)(void), void (*setup)(void), int timeout);
+            void ADD_TEST_TEARDOWN_C(const char* suite, const char* test, void (*func)(void), void (*teardown)(void));
+            void ADD_TEST_TEARDOWN_TIMEOUT_C(const char* suite, const char* test, void (*func)(void), void (*teardown)(void), int timeout);
+            void ADD_TEST_SETUP_TEARDOWN_TIMEOUT_C(const char* suite, const char* test, void (*func)(void), void (*setup)(void), void (*teardown)(void), int timeout);
         #ifdef
     }
 #endif

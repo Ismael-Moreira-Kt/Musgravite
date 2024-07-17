dde075ed56d1e10e47f4f9def63634c36b98fc77
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-17 22:46:05 +0100

    feat: Adding the test header.
    I have declared the two functions that will be used for the example.

---
+++ docs/C/examples/matrix_multiplication/matrix_multiplication.h
@@ -0,0 +1,15 @@
+#ifndef MATRIX_MULTIPLICATION_H
+#define MATRIX_MULTIPLICATION_H
+
+
+
+
+
+void multiplyMatrices(int firstMatrix[][10], int secondMatrix[][10], int result[][10], int rowFirst, int columnFirst, int rowSecond, int columnSecond);
+void displayMatrix(int matrix[][10], int row, int column);
+
+
+
+
+
+#endif

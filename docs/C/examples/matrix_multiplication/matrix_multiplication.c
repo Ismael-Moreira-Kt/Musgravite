0fc7aa8df7a798693d6cb3f4952509af3a879a52
Ismael-Moreira-Kt <ismaelmoreiraubuntu@gmail.com>
2024-07-17 22:47:57 +0100

    feat: Adding the test implementation.
    I've added the implementation of the multiplyMatrices function.

---
+++ docs/C/examples/matrix_multiplication/matrix_multiplication.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "matrix_multiplication.h"
+
+
+
+
+
+void multiplyMatrices(int firstMatrix[][10], int secondMatrix[][10], int result[][10], int rowFirst, int columnFirst, int rowSecond, int columnSecond) {
+    for (int i = 0; i < rowFirst; i++) {
+        for (int j = 0; j < columnSecond; j++) {
+            result[i][j] = 0;
+        }
+    }
+
+
+    for (int i = 0; i < rowFirst; i++) {
+        for (int j = 0; j < columnSecond; j++) {
+            for (int k = 0; k < columnFirst; k++) {
+                result[i][j] += firstMatrix[i][k] * secondMatrix[k][j];
+            }
+        }
+    }
+}
+
+
+
+
+
